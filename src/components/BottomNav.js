c2596c3e0dc5f2c4572c3beb514261f528029c67
jineshnagori <jineshn21@gmail.com>
2021-07-12 23:52:04 +0530

    bottom nav fix

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BottomNav() {
     return (
         <>
-            <div className="bottom-nav fixed-nav">
+            <div className="bottom-nav fixed-bottom-nav">
                 <ul>
                     <li className="mr"><a href="#projects"><i className="fa fa-tasks"></i><span>Projects</span></a></li>
                     <li className="mr"><a href="#about"><i className="fa fa-user"></i><span>About</span></a></li>
