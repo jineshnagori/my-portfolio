ccd94fa9048e6a9358fc7a52bf26ac42bfd2492d
jineshnagori <jineshn21@gmail.com>
2021-11-05 15:06:06 +0530

    Updates

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,3 +1,17 @@
+function toggleDarkMode() {
+  const container = document.getElementById('simulateDarkMode');
+  const buttonDarkMode = document.getElementById('toggleDarkMode');
+  const dataTheme = container.getAttribute('data-theme');
+
+  if (dataTheme === 'dark') {
+      container.setAttribute('data-theme', 'light');
+      buttonDarkMode.innerHTML = 'DARK MODE';
+  } else {
+      container.setAttribute('data-theme', 'dark');
+      buttonDarkMode.innerHTML = 'LIGHT MODE';
+  }
+}
+
 (function ($) {
   "use strict";
 
