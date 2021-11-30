e61c051a0704fc650c1efb7d9a8676538e212acf
jineshnagori <jineshn21@gmail.com>
2021-11-30 17:49:49 +0530

    css in toggle button

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function ToggleButton() {
     return (
         <>
-            <div id="toggleDarkMode" className="btn-toggle">
-                <button className="toggle_wrap">
-                    <i className="fa text-white fa-moon-o show"></i>
-                    <i className="fa fa-sun-o hide"></i>
-                </button>
+            <div id="toggleDarkMode">
+                <div className="toggle_wrap">
+                    <i className="moon fa fa-moon-o show"></i>
+                    <i className="sun fa fa-sun-o hide"></i>
+                </div>
             </div>
         </>
     )
