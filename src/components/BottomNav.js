44d2b059ffc6e0a2c104c51a3b4e28888a63af18
jineshnagori <jineshn21@gmail.com>
2022-01-01 21:42:43 +0530

    bottom nav for mobile changed

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -3,12 +3,17 @@ import React from 'react'
 export default function BottomNav() {
     return (
         <>
-            <div className="bottom-nav fixed-bottom-nav">
-                <div><a href="#projects"><i className="fa fa-tasks"></i><span>Projects</span></a></div>
-                <div><a href="#about"><i className="fa fa-user"></i><span>About</span></a></div>
-                <div><a href="#"><i className="fa fa-home"></i><span>Home</span></a></div>
-                <div><a href="#blog"><i className="fa fa-book"></i><span>Blogs</span></a></div>
-                <div><a href="#contact"><i className="fa fa-phone"></i><span>Contact</span></a></div>
+            <div className="bottom-nav">
+                <div className="bottom-nav-items">
+                    <a className="bottom-nav-item" href="#projects"><i className="fa fa-tasks"></i><span>Projects</span></a>
+                    <a className="bottom-nav-item" href="#about"><i className="fa fa-user"></i><span>About</span></a>
+                    <div id="toggleMode">
+                        <i id="toggleIcon" className="moon fa fa-moon-o show"></i>
+                        <i id="toggleIcon" className="sun fa fa-sun-o hide"></i>
+                    </div>
+                    <a className="bottom-nav-item" href="#blog"><i className="fa fa-book"></i><span>Blogs</span></a>
+                    <a className="bottom-nav-item" href="#contact"><i className="fa fa-phone"></i><span>Contact</span></a>
+                </div>
             </div>
         </>
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <>
             <footer>
-                <p className="footer-center">Copyright &copy; 2021 Jinesh Nagori. All Rights Reserved.</p>
+                <p className="footer-center">Copyright &copy; 2021 Jinesh Nagori.<br />All Rights Reserved.</p>
             </footer>
         </>
     )
