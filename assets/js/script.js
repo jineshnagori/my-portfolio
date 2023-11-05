1e9b71aeeb466872d509d427ba590026042d6431
jineshnagori <jineshn21@gmail.com>
2023-11-05 20:41:58 +0530

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,15 @@ $(window).on("scroll", function () {
   }
 });
 
-$("body").on("click", "i", function (e) {
+$("body").on("click", "#toggleIcon", function (e) {
   setTimeout(function () {
     $(".show, .hide").toggle();
   }, 100);
+});
+
+$("body").on("click", ".project-item", function (e) {
+  const urls = ["https://amitcomputers.in", "https://vanguard-united.com", "https://socketos.eu.org", "https://workseez.com", "https://jineshnagori.github.io/xccurate-security/", "https://jineshnagori.github.io/manorama/", "https://jineshnagori.github.io/akshatsingh.ml/", "https://github.com/jineshnagori/mad-music", "https://github.com/jineshnagori/FreeBookPost", "https://github.com/jineshnagori/easyRecharge"];
+  const index = $(this).index();
+  const url = urls[index];
+  window.open(url, "_blank");
 });
